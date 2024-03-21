4b6c28a5e13c23502c3e01f266887ba2055fa95c
DANIEL <158509534+DANIEL-UEL@users.noreply.github.com>
2024-03-21 11:01:43 +0000

    Frontend

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https:shiny-couscous-646vg7p5x4j24j7v-3000.app.github.dev/";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
